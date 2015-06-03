a2fc676f051e21e5a65dcb5904cdcde4bc6d4895
jsk0301 <johnk.395@gmail.com>
2015-06-03 14:00:23 -0400

    Created GUIs
    Tank and Options panels combined, need to edit for better cohesion

---
+++ Final Project/OptionsPanel.java
@@ -0,0 +1,88 @@
+import javax.swing.*;                        //import GUI
+import java.awt.*;                        //import graphics
+import java.awt.event.*;                    //import listeners
+public class OptionsPanel extends JPanel
+{                                    //fields
+   private JLabel tankChoice;                    //shows tank selection
+   private JButton lightButton, mediumButton, heavyButton;
+   private Tank tankType;                    //sets selection
+   public OptionsPanel()
+   {
+      /*
+      Instantiate 3 buttons for each tank type
+    Each button sets tankType to their respective tanks
+      Add same TankTypeListener to each
+    Instantiate “Play” button that activates after selection
+   */
+      setLayout(new BorderLayout());
+      
+      tankChoice = new JLabel("Choose your tank.");
+      add(tankChoice, BorderLayout.NORTH);
+      
+      lightButton = new JButton("Light Tank");
+      lightButton.addActionListener(new TankTypeListener("light"));
+      add(lightButton, BorderLayout.CENTER);
+      
+      mediumButton = new JButton("Medium Tank");
+      mediumButton.addActionListener(new TankTypeListener("medium"));
+      add(mediumButton, BorderLayout.CENTER);
+      
+      heavyButton = new JButton("Heavy Tank");
+      heavyButton.addActionListener(new TankTypeListener("heavy"));
+      add(heavyButton, BorderLayout.CENTER);
+      
+      JButton playButton = new JButton("Click to play.");
+      playButton.addActionListener(new PlayListener());
+      playButton.setEnabled(false);
+      add(playButton, BorderLayout.SOUTH);
+   }
+   public Tank getTankType()                    //tank type getter
+   {
+      return tankType;
+   }
+   private class TankTypeListener implements ActionListener
+   {
+      private String myTank;
+      public TankTypeListener(String name)
+      {
+         myTank = name;
+      }
+      public void actionPerformed(ActionEvent e)
+      {
+       /*
+      Determines which tankType was chosen
+      Sets JLabel appropriately
+      Enables “Play” button
+         */
+         if(myTank.equals("light"))
+         {
+            tankType = new LightTank();
+         }
+         
+         if(myTank.equals("medium"))
+         {
+            tankType = new MediumTank();
+         }
+         
+         if(myTank.equals("heavy"))
+         {
+            tankType = new HeavyTank();
+         }
+         tankChoice.setText(myTank);
+      }
+   }
+   private class PlayListener implements ActionListener
+   {
+      public void actionPerformed(ActionEvent e)
+      {
+      /*
+      Disable all buttons
+      Start TankPanel.start() method
+      */
+         lightButton.setEnabled(false);
+         mediumButton.setEnabled(false);
+         heavyButton.setEnabled(false);
+         TankPanel.start();
+      }
+   }
+}
