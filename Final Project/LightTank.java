ce6ff095291051392d3128ee2c8b4bd2462a0019
michaelstepniczka <mstepniczka2@gmail.com>
2015-06-04 23:29:18 -0400

    Update LightTank.java

--- Final Project/LightTank.java
+++ Final Project/LightTank.java
@@ -3,13 +3,13 @@ public class LightTank extends Tank
 {   
    public LightTank()
    {
-      super(100, 10, new LightWeapon(), 300, 300);
+      super(100, 10, new LightWeapon(), 300, 300); //creates lightTank
       setIcon("lighttank0.png");
    }
    
    public LightTank(int x, int y)
    {
-      super(100, 10, new LightWeapon(), x, y);
+      super(100, 10, new LightWeapon(), x, y);   //creates lightTank in given position
       setIcon("lighttank0.png");
    }
    
