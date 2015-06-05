11bc9a2db93d94a499360e8ce982dc7cf182c599
michaelstepniczka <mstepniczka2@gmail.com>
2015-06-04 23:30:56 -0400

    Update MediumTank.java

--- Final Project/MediumTank.java
+++ Final Project/MediumTank.java
@@ -3,13 +3,13 @@ public class MediumTank extends Tank
 {   
    public MediumTank()
    {
-      super(125, 7, new MediumWeapon(), 300, 300);          //center
+      super(125, 7, new MediumWeapon(), 300, 300);          //default constructor
       setIcon("mediumtank0.png");
    }
    
    public MediumTank(int x, int y)
    {
-      super(125, 7, new MediumWeapon(), x, y);
+      super(125, 7, new MediumWeapon(), x, y);     //creates mediumTank in given location
       setIcon("mediumtank0.png");
    }
    
