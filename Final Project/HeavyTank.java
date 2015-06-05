d469480bfeaf5901b5d86bdee76238591c8e09a6
michaelstepniczka <mstepniczka2@gmail.com>
2015-06-04 23:30:04 -0400

    Update HeavyTank.java

--- Final Project/HeavyTank.java
+++ Final Project/HeavyTank.java
@@ -1,13 +1,13 @@
 import java.awt.*;
 public class HeavyTank extends Tank
 {
-   public HeavyTank()
+   public HeavyTank()   //default constructor
    {
       super(150, 5, new HeavyWeapon(), 300, 300);
       setIcon("heavytank0.png");
    }
    
-   public HeavyTank(int x, int y)
+   public HeavyTank(int x, int y)   //2-arg constructor for positioning heavyTank
    {
       super(150, 5, new HeavyWeapon(), x, y);
       setIcon("heavytank0.png");
